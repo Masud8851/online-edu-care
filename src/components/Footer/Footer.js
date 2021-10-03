8a6e59c902337f9cf24a6262700cf96d4c55f6b5
Masud8851 <masud1971.cse@gmail.com>
2021-10-04 03:16:08 +0600

    Add

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+	return (
+		<div>
+			<footer className="row">
+				<div className="col-md-3">
+					<h1>Footer</h1>
+				</div>
+			</footer>
+		</div>
+	);
+};
+
+export default Footer;
