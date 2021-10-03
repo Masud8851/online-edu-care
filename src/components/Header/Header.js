7ea79976a94ebc32fad8da85a85a3aea74709145
Masud8851 <masud1971.cse@gmail.com>
2021-10-04 03:02:23 +0600

    Add

---
+++ src/components/Header/Header.js
@@ -0,0 +1,54 @@
+import React from "react";
+import "../Header/Header.css";
+import logo from "../../images/logo.png";
+
+const Header = () => {
+	return (
+		<div className="header">
+			<div>
+				<nav class="navbar navbar-expand-lg navbar-light bg-light">
+					<img src={logo} alt="" />
+					<div class="container-fluid">
+						<button
+							class="navbar-toggler"
+							type="button"
+							data-bs-toggle="collapse"
+							data-bs-target="#navbarSupportedContent"
+							aria-controls="navbarSupportedContent"
+							aria-expanded="false"
+							aria-label="Toggle navigation"
+						>
+							<span class="navbar-toggler-icon"></span>
+						</button>
+						<div class="collapse navbar-collapse" id="navbarSupportedContent">
+							<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+								<li class="nav-item">
+									<a class="nav-link active" aria-current="page" href="/">
+										Home
+									</a>
+								</li>
+								<li class="nav-item">
+									<a class="nav-link active" aria-current="page" href="/">
+										About
+									</a>
+								</li>
+								<li class="nav-item">
+									<a class="nav-link active" aria-current="page" href="/">
+										Services
+									</a>
+								</li>
+								<li class="nav-item">
+									<a class="nav-link active" aria-current="page" href="/">
+										Blog
+									</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</nav>
+			</div>
+		</div>
+	);
+};
+
+export default Header;
