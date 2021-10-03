57a46fc5780e9dcdcfc77ff71c9052b3a77e9be7
Masud8851 <masud1971.cse@gmail.com>
2021-10-04 03:20:11 +0600

    Add

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,17 @@
 import React from "react";
+import "../Footer/Footer.css";
 
 const Footer = () => {
 	return (
 		<div>
 			<footer className="row">
-				<div className="col-md-3">
+				<div className="col-md-4">
+					<h1>Footer</h1>
+				</div>
+				<div className="col-md-4">
+					<h1>Footer</h1>
+				</div>
+				<div className="col-md-4">
 					<h1>Footer</h1>
 				</div>
 			</footer>
