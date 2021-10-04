128e4c81ea2fb4885c86927912990f92e18d454e
Masud8851 <masud1971.cse@gmail.com>
2021-10-04 13:26:52 +0600

    Add

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -2,6 +2,7 @@ import React from "react";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
 import banner from "../../images/blogs/blog.jpg";
+import blog1 from "../../images/blogs/blog-1.jpg";
 
 const Blogs = () => {
 	return (
@@ -10,7 +11,16 @@ const Blogs = () => {
 			<div>
 				<img src={banner} alt="" className="w-100" height="500" />
 			</div>
-			<div className="row"></div>
+			<div className="row">
+				<div className="col-md-7 p-3">
+					<div>
+						<div>
+							<img src={blog1} alt="" className="w-100" />
+						</div>
+					</div>
+				</div>
+				<div className="col-md-5 p-3"></div>
+			</div>
 			<Footer></Footer>
 		</div>
 	);
