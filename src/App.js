3463c02b636ad8d5baf5910982399679f9630f13
Masud8851 <masud1971.cse@gmail.com>
2021-10-04 14:23:24 +0600

    Add

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
-import logo from "./logo.svg";
 import "./App.css";
-import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
 import Home from "./components/Home/Home";
-import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import About from "./components/About/About";
 import Services from "./components/Services/Services";
