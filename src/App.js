be8bb7604d60b4e874dc10af0158bfd0deeeec8c
Masud8851 <masud1971.cse@gmail.com>
2021-10-04 23:38:44 +0600

    Add

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import "./App.css";
 import Home from "./components/Home/Home";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import About from "./components/About/About";
-import Services from "./components/Services/Services";
 import Blogs from "./components/Blogs/Blogs";
 import NotFound from "./components/NotFound/NotFound";
 import { useEffect, useState } from "react";
