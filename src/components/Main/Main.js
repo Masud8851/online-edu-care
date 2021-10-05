1dcac1aaab41f029bc1fa268450cc219fd19958f
Masud8851 <masud1971.cse@gmail.com>
2021-10-05 09:06:11 +0600

    ActiveStyle

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
 import Services from "../Services/Services";
-
+import { Animated } from "react-animated-css";
 import banner from "../../images/services-banner.jpg";
 
 const Main = () => {
@@ -20,9 +20,12 @@ const Main = () => {
 				<img src={banner} alt="" className="w-100" height="400" />
 			</div>
 
-			<div className="text-center mt-5">
-				<h1 className="text-success fs-1">Available Courses</h1>
-			</div>
+			<Animated animationIn="rollIn" animationOut="tada" isVisible={true}>
+				<div className="text-center mt-5">
+					<h1 className="text-success fs-1">[ Available Courses ]</h1>
+				</div>
+			</Animated>
+
 			<div className="row row-cols-1 row-cols-md-3 g-4 pt-5">
 				{courses.map((course) => (
 					<Services
