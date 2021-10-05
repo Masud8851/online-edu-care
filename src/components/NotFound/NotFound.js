eb9e365fe0d677b003eec874496867da46546fa0
Masud8851 <masud1971.cse@gmail.com>
2021-10-05 15:08:48 +0600

    Error Section

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -3,13 +3,23 @@ import "./NotFound.css";
 
 const NotFound = () => {
 	return (
-		<div className="text-center error">
-			<div className="d-flex flex-column align-items-center">
-				<i class="fas fa-bomb fs-1"></i>
-				<p>404</p>
-				<p>Not Found</p>
+			<div id="notfound">
+		<div class="notfound">
+			<div class="notfound-404">
+				<h1>Oops!</h1>
 			</div>
+			<h2>404 - Page not found</h2>
+			<p>The page you are looking for might have been removed had its name changed or is temporarily unavailable.</p>
+			<a href="#">Go To Homepage</a>
 		</div>
+	</div>
+		// <div className="text-center error">
+		// 	<div className="d-flex flex-column align-items-center">
+		// 		<i class="fas fa-bomb fs-1"></i>
+		// 		<p>404</p>
+		// 		<p>Not Found</p>
+		// 	</div>
+		// </div>
 	);
 };
 
