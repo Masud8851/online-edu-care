8cfc481b26ed5976065d0eba6ac694c2eaace618
Masud8851 <masud1971.cse@gmail.com>
2021-10-05 15:10:19 +0600

    Error Section

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,25 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./NotFound.css";
 
 const NotFound = () => {
 	return (
-			<div id="notfound">
-		<div class="notfound">
-			<div class="notfound-404">
-				<h1>Oops!</h1>
+		<div id="notfound">
+			<div class="notfound">
+				<div class="notfound-404">
+					<h1>Oops!</h1>
+				</div>
+				<h2>404 - Page not found</h2>
+				<p>
+					The page you are looking for might have been removed had its name
+					changed or is temporarily unavailable.
+				</p>
+				<Link to="/">Go To Homepage</Link>
 			</div>
-			<h2>404 - Page not found</h2>
-			<p>The page you are looking for might have been removed had its name changed or is temporarily unavailable.</p>
-			<a href="#">Go To Homepage</a>
 		</div>
-	</div>
-		// <div className="text-center error">
-		// 	<div className="d-flex flex-column align-items-center">
-		// 		<i class="fas fa-bomb fs-1"></i>
-		// 		<p>404</p>
-		// 		<p>Not Found</p>
-		// 	</div>
-		// </div>
 	);
 };
 
